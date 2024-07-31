e74c9c60062fbd5306b62c56722c2ac3fe5b55e3
DFuentesOrtez <fuentesortezdavid@gmail.com>
2024-07-31 06:22:57 -0400

    VERY INITIAL COMMIT

---
+++ script.js
@@ -0,0 +1,16 @@
+var x = document.getElementsByClassName("movie");
+x[1].setAttribute("style", "flex-direction: row-reverse");
+x[3].setAttribute("style", "flex-direction: row-reverse");
+x[5].setAttribute("style", "flex-direction: row-reverse");
+x[7].setAttribute("style", "flex-direction: row-reverse");
+x[9].setAttribute("style", "flex-direction: row-reverse");
+x[11].setAttribute("style", "flex-direction: row-reverse");
+x[13].setAttribute("style", "flex-direction: row-reverse");
+x[15].setAttribute("style", "flex-direction: row-reverse");
+x[17].setAttribute("style", "flex-direction: row-reverse");
+x[19].setAttribute("style", "flex-direction: row-reverse");
+x[21].setAttribute("style", "flex-direction: row-reverse");
+x[23].setAttribute("style", "flex-direction: row-reverse");
+x[25].setAttribute("style", "flex-direction: row-reverse");
+x[27].setAttribute("style", "flex-direction: row-reverse");
+x[29].setAttribute("style", "flex-direction: row-reverse");
