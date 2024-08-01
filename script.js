a273508f8aa7890ad69846842e2dd3a6f029e265
DFuentesOrtez <fuentesortezdavid@gmail.com>
2024-08-01 00:52:28 -0400

    Making main content scrollable

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-var x = document.getElementsByClassName("movie");
-x[1].setAttribute("style", "flex-direction: row-reverse");
-x[3].setAttribute("style", "flex-direction: row-reverse");
-x[5].setAttribute("style", "flex-direction: row-reverse");
-x[7].setAttribute("style", "flex-direction: row-reverse");
-x[9].setAttribute("style", "flex-direction: row-reverse");
-x[11].setAttribute("style", "flex-direction: row-reverse");
-x[13].setAttribute("style", "flex-direction: row-reverse");
-x[15].setAttribute("style", "flex-direction: row-reverse");
-x[17].setAttribute("style", "flex-direction: row-reverse");
-x[19].setAttribute("style", "flex-direction: row-reverse");
-x[21].setAttribute("style", "flex-direction: row-reverse");
-x[23].setAttribute("style", "flex-direction: row-reverse");
-x[25].setAttribute("style", "flex-direction: row-reverse");
-x[27].setAttribute("style", "flex-direction: row-reverse");
-x[29].setAttribute("style", "flex-direction: row-reverse");
+let mouseDown = false;
+let startX, scrollLeft;
+const slider = document.querySelector(".fullList");
+
+const startDragging = (e) => {
+    mouseDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+}
+
+const stopDragging = (e) => {
+    mouseDown = false;
+}
+
+const move = (e) => {
+    e.preventDefault();
+    if (!mouseDown) { return; }
+    const x = e.pageX - slider.offsetLeft;
+    const scroll = x - startX;
+    slider.scrollLeft = scrollLeft - scroll;
+}
+
+// Add the event listeners
+slider.addEventListener('mousemove', move, false);
+slider.addEventListener('mousedown', startDragging, false);
+slider.addEventListener('mouseup', stopDragging, false);
+slider.addEventListener('mouseleave', stopDragging, false);
