e6cf3db1f37e1bfe826da6e09c667f6edcf65a0e
DFuentesOrtez <fuentesortezdavid@gmail.com>
2024-08-01 19:46:50 -0400

    Minor changes

--- script.js
+++ script.js
@@ -217,34 +217,6 @@ $(document).ready(function () {
 
 
 
-// let mouseDown = false;
-// let startX, scrollLeft;
-// const slider = document.querySelector(".fullList");
-
-// const startDragging = (e) => {
-//     mouseDown = true;
-//     startX = e.pageX - slider.offsetLeft;
-//     scrollLeft = slider.scrollLeft;
-// }
-
-// const stopDragging = (e) => {
-//     mouseDown = false;
-// }
-
-// const move = (e) => {
-//     e.preventDefault();
-//     if (!mouseDown) { return; }
-//     const x = e.pageX - slider.offsetLeft;
-//     const scroll = x - startX;
-//     slider.scrollLeft = scrollLeft - scroll;
-// }
-
-// // Add the event listeners
-// slider.addEventListener('mousemove', move, false);
-// slider.addEventListener('mousedown', startDragging, false);
-// slider.addEventListener('mouseup', stopDragging, false);
-// slider.addEventListener('mouseleave', stopDragging, false);
-
 const container = document.querySelector('.fullList');
 
 let startY;
@@ -287,15 +259,10 @@ function mouseMove(e) {
     }
 }
 
+
+
 let horizontal = document.getElementById("myList");
 horizontal.addEventListener("wheel", (e) => {
     e.preventDefault();
     horizontal.scrollLeft += e.deltaY;
 })
-
-// var item = document.getElementById("myList");
-
-// window.addEventListener("wheel", function (e) {
-//     if (e.deltaY > 0) item.scrollLeft += 100;
-//     else item.scrollLeft -= 100;
-// });
